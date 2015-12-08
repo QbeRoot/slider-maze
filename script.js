0b4e9e480ca47a0fa1752acd1dc24f62bb1d8831
QbeRoot <qbe.root@gmail.com>
2015-12-08 01:55:54 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var select = document.getElementById('level'),
+	slider = document.getElementById('slider'),
+	levels = [
+	function (x) { return x },
+	function (x) { return x + 1 }
+]
+
+for (var i = 0, l = levels.length; i < l; ++i) {
+	var option = document.createElement('option')
+	option.setAttribute('value', i)
+	option.appendChild(document.createTextNode(i))
+	select.appendChild(option)
+}
+
+select.addEventListener('change', function () {
+	slider.value = 0
+})
+
+slider.addEventListener('change', function () {
+	slider.value = levels[select.value](+slider.value)
+})
